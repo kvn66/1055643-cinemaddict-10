b45ed25d74beee687870461dcc02c8cc4777b840
Vadim Kalashnikov <vadim.kalashnikov66@gmail.com>
2019-12-04 17:02:12 +0500

    Update

--- src/components/film.js
+++ src/components/film.js
@@ -8,25 +8,26 @@ export const createFilmElement = (film) => {
   const footer = document.querySelector(`.footer`);
   const cardElement = filmCard.getElement();
   const detailsElement = filmDetails.getElement();
+  const closeButton = detailsElement.querySelector(`.film-details__close-btn`);
 
   const onEscKeyDown = (evt) => {
     const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
 
     if (isEscKey) {
       document.querySelector(`.film-details`).remove();
-      document.removeEventListener(`click`, onCloseClick);
+      closeButton.removeEventListener(`click`, onCloseClick);
       document.removeEventListener(`keydown`, onEscKeyDown);
     }
   };
 
   const onCloseClick = () => {
     document.querySelector(`.film-details`).remove();
-    document.removeEventListener(`click`, onCloseClick);
+    closeButton.removeEventListener(`click`, onCloseClick);
     document.removeEventListener(`keydown`, onEscKeyDown);
   };
 
   const onClick = () => {
-    detailsElement.querySelector(`.film-details__close-btn`).addEventListener(`click`, onCloseClick);
+    closeButton.addEventListener(`click`, onCloseClick);
     render(footer, detailsElement, RenderPosition.AFTEREND);
     document.addEventListener(`keydown`, onEscKeyDown);
   };
