6ffd2e1942381cb2907bd0df9cf0b1c18fa828e9
Vadim Kalashnikov <vadim.kalashnikov66@gmail.com>
2019-12-23 17:01:22 +0500

    Подключил API

--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
+import CodeRain from 'coderain';
+import API from './api';
 import PageController from './controllers/page';
 import MoviesModel from './models/movies';
 import {generateFilms} from './mock/films';
 
+const cr = new CodeRain(`###################`);
+const AUTHORIZATION = `Basic ` + cr.next();
+const END_POINT = `https://htmlacademy-es-10.appspot.com/cinemaddict`;
+
 
 const MOVIES_COUNT = 22;
+
+const api = new API(END_POINT, AUTHORIZATION);
 
 const movies = generateFilms(MOVIES_COUNT);
 const moviesModel = new MoviesModel(movies);
 
 new PageController(document, moviesModel).render();
+
+api.getTasks()
+  .then((tasks) => {
+    console.log(tasks);
+  });
+
+console.log(AUTHORIZATION);
+
