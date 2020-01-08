4f5ebcc8fad65dbb2cbe3b123b9a50eb7f3e6255
kvn66 <vadim.kalashnikov66@gmail.com>
2020-01-08 21:14:57 +0500

    Добавил Service Worker

--- src/main.js
+++ src/main.js
@@ -6,9 +6,25 @@ import MoviesModel from './models/movies';
 const AUTHORIZATION = `Basic ` + new CodeRain(`###################`).next();
 const END_POINT = `https://htmlacademy-es-10.appspot.com/cinemaddict`;
 
+window.addEventListener(`load`, () => {
+  navigator.serviceWorker.register(`/sw.js`)
+    .then(() => {
+      // Действие, в случае успешной регистрации ServiceWorker
+    }).catch(() => {
+    // Действие, в случае ошибки при регистрации ServiceWorker
+    });
+});
 
 const api = new API(END_POINT, AUTHORIZATION);
 
 const moviesModel = new MoviesModel();
 
 new PageController(moviesModel, api).render(document);
+
+window.addEventListener(`online`, () => {
+  document.title = document.title.replace(` [offline]`, ``);
+});
+
+window.addEventListener(`offline`, () => {
+  document.title += ` [offline]`;
+});
